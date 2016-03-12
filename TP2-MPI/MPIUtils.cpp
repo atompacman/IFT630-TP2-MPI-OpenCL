a474bc86055ffd67515e7fc2c0e6463873992d14
atompacman <jeremiecoul@hotmail.com>
2016-03-12 13:03:22 -0500

    1. a) - Done

--- TP2-MPI/MPIUtils.cpp
+++ TP2-MPI/MPIUtils.cpp
@@ -1,64 +1,31 @@
 #include "MPIUtils.h"
+#include <mpi.h>
 
-namespace MPIUtils
+void runManagerWorkerAlgorithm(int argc, char ** argv, MPIFunc i_ManagerFunc, MPIFunc i_WorkerFunc)
 {
-    void runMasterWorkerAlgorithm(int argc, char** argv, MPIFunc i_MasterFunc, MPIFunc i_WorkerFunc)
+    // Initialize MPI
+    int flag;
+    MPI_Initialized(&flag);
+    if (!flag)
     {
-        // Initialize MPI
-        int flag;
-        MPI_Initialized(&flag);
-        if (!flag)
-        {
-            MPI_Init(&argc, &argv);
-        }
-
-        // Get number of running processes
-        int numProcesses;
-        MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
-
-        // Get ID of current process
-        int processID;
-        MPI_Comm_rank(MPI_COMM_WORLD, &processID);
-
-        // Run correct code
-        (processID == 0 ? i_MasterFunc : i_WorkerFunc)(processID, numProcesses);
-
-        // Finalize MPI
-        MPI_Finalized(&flag);
-        if (!flag)
-        {
-            MPI_Finalize();
-        }
+        MPI_Init(&argc, &argv);
     }
 
-    void sendToWorker(void const * i_Buf,
-                      uint32_t     i_Count,
-                      MPI_Datatype i_Type,
-                      uint32_t     i_WorkerID)
-    {
-        MPI_Send(i_Buf, i_Count, i_Type, i_WorkerID, 0, MPI_COMM_WORLD);
-    }
+    // Get number of running processes
+    int numProcesses;
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 
-    void receiveFromAnyWorker(void *       i_Buf,
-                              uint32_t     i_Count,
-                              MPI_Datatype i_Type,
-                              MPI_Status * i_Status)
-    {
-        MPI_Recv(i_Buf, i_Count, i_Type, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, i_Status);
-    }
+    // Get ID of current process
+    int processID;
+    MPI_Comm_rank(MPI_COMM_WORLD, &processID);
 
-    void sendToMaster(void const * i_Buf,
-                      uint32_t     i_Count,
-                      MPI_Datatype i_Type)
-    {
-        MPI_Send(i_Buf, i_Count, i_Type, 0, 0, MPI_COMM_WORLD);
-    }
+    // Run correct code
+    (processID == MANAGER_ID ? i_ManagerFunc : i_WorkerFunc)(processID, numProcesses);
 
-    void receiveFromMaster(void *       i_Buf,
-                           uint32_t     i_Count,
-                           MPI_Datatype i_Type)
+    // Finalize MPI
+    MPI_Finalized(&flag);
+    if (!flag)
     {
-        MPI_Recv(i_Buf, i_Count, i_Type, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Finalize();
     }
-
 }
--- TP2-MPI/MPIUtils.h
+++ TP2-MPI/MPIUtils.h
@@ -2,32 +2,12 @@
 #define MPIUTILS
 
 #include <functional>
-#include <mpi.h>
 #include <stdint.h>
 
-namespace MPIUtils
-{
-    typedef std::function<void(uint32_t i_CurrProc, uint32_t i_NumProc)> MPIFunc;
+uint32_t const MANAGER_ID = 0;
 
-    void runMasterWorkerAlgorithm(int argc,char** argv, MPIFunc i_MasterFunc, MPIFunc i_WorkerFunc);
+typedef std::function<void(uint32_t i_CurrProc, uint32_t i_NumProc)> MPIFunc;
 
-    void sendToWorker(void const * i_Buf,
-                      uint32_t     i_Count,
-                      MPI_Datatype i_Type,
-                      uint32_t     i_WorkerID);
-
-    void receiveFromAnyWorker(void *       i_Buf,
-                              uint32_t     i_Count,
-                              MPI_Datatype i_Type,
-                              MPI_Status * i_Status = MPI_STATUS_IGNORE);
-
-    void sendToMaster(void const * i_Buf,
-                      uint32_t     i_Count,
-                      MPI_Datatype i_Type);
-
-    void receiveFromMaster(void *       i_Buf,
-                           uint32_t     i_Count,
-                           MPI_Datatype i_Type);
-}
+void runManagerWorkerAlgorithm(int argc, char ** argv, MPIFunc i_ManagerFunc, MPIFunc i_WorkerFunc);
 
 #endif //MPIUTILS
