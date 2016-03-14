99ba0fe110d259342bed48bbc025c489e55aafdc
alexblouin <alex.blouin64@gmail.com>
2016-03-14 00:23:58 -0400

    Added a method to compute the norm difference between matrices, and use it to check when to stop looping. Now I just need to find out why the final messages aren't being sent and received properly.

--- TP2-MPI/Jacobi.cpp
+++ TP2-MPI/Jacobi.cpp
@@ -3,10 +3,7 @@
 #include <iomanip>
 #include <iostream>
 #include <mpi.h>
-
-//uint32_t    const MATRIX_SIZE = 20;
-//uint32_t    const NUM_ELEM = MATRIX_SIZE * MATRIX_SIZE;
-//MPI_Request const ON_VACATION = std::numeric_limits<MPI_Request>().max();
+#include <cmath>
 
 static void printSquareMatrix(double * i_Matrix, int i_NumRows, char const * i_Name)
 {
@@ -22,38 +19,20 @@ static void printSquareMatrix(double * i_Matrix, int i_NumRows, char const * i_N
 	std::cout << std::endl;
 }
 
-//void sendJob(double      * i_MatrixA,
-//             double      * i_MatrixBt,
-//             double      * i_Results,
-//             double      * i_RowsBuf,
-//             MPI_Request * i_PendingRequests,
-//             uint32_t      i_MatrixComponent,
-//             uint32_t      i_ProcessID)
-//{
-//    // Compute position in matrices where concerned rows are
-//    auto row = i_MatrixComponent / MATRIX_SIZE;
-//    auto col = i_MatrixComponent % MATRIX_SIZE;
-//
-//    // Copy matrix rows in the row buffer
-//    memcpy(i_RowsBuf, i_MatrixA + row * MATRIX_SIZE, MATRIX_SIZE * sizeof(double));
-//    memcpy(i_RowsBuf + MATRIX_SIZE, i_MatrixBt + col * MATRIX_SIZE, MATRIX_SIZE * sizeof(double));
-//
-//    // Send row buffers to worker
-//    MPI_Send(i_RowsBuf, 2 * MATRIX_SIZE, MPI_DOUBLE, i_ProcessID, 0, MPI_COMM_WORLD);
-//
-//    // Post non-blocking receive to be ready for result reception
-//    MPI_Request request;
-//    auto *      result = i_Results + i_MatrixComponent;
-//    MPI_Irecv(result, 1, MPI_DOUBLE, i_ProcessID, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
-//    i_PendingRequests[i_ProcessID - 1] = request;
-//}
 
-//void sendOnVacation(uint32_t i_ProcessID, MPI_Request * i_PendingRequests)
-//{
-//    double dummy[2 * MATRIX_SIZE];
-//    MPI_Send(dummy, 2 * MATRIX_SIZE, MPI_DOUBLE, i_ProcessID, ON_VACATION, MPI_COMM_WORLD);
-//    i_PendingRequests[i_ProcessID - 1] = ON_VACATION;
-//}
+double computeSquaredNormDifference(double *oldMatrix, double *newMatrix, int size) 
+{
+	double squaredNorm = 0.0;
+	for (auto y = 1; y < size - 1; ++y)
+	{
+		for (auto x = 1; x < size - 1; ++x)
+		{
+			squaredNorm += (newMatrix[y * size + x] - oldMatrix[y * size + x]) * (newMatrix[y * size + x] - oldMatrix[y * size + x]);
+		}
+		
+	}
+	return squaredNorm;
+}
 
 void runManager(uint32_t, uint32_t i_NumProc)
 {
@@ -63,6 +42,7 @@ void runManager(uint32_t, uint32_t i_NumProc)
 	// Create matrix
 	auto numRows = numProcesses + 1;
 	auto * matrix = new double[numRows * numRows];
+	auto * oldMatrix = new double[numRows * numRows];
 
 	for (auto y = 0U; y < numRows; ++y)
 	{
@@ -82,24 +62,53 @@ void runManager(uint32_t, uint32_t i_NumProc)
 	do
 	{
 		++iter;
+		std::cout << "Now starting iteration #" << iter << std::endl;
 		difference = 0.0;
+		// "Save" the current matrix before it gets changed by the computation in order to compare the norm difference to an epsilon
+		memcpy(oldMatrix, matrix, numRows * numRows * sizeof(double));
+
+		std::cout << "Matrix copied. About to distribute the jacobi computation... " << std::endl;
+
 		// Send rows to workers
 		for (auto y = 0U; y < numRows - 2; ++y)
 		{
 			MPI_Send(matrix + y * numRows, 3 * numRows, MPI_DOUBLE, y+1, 0, MPI_COMM_WORLD);
 		}
 
+		std::cout << "Work sent. About to receive... " << std::endl;
+
+		// Receive the resulting rows from the workers
 		for (auto y = 1U; y < numRows - 1; ++y)
 		{
 			MPI_Recv(matrix + 1 + y * numRows, numRows - 2, MPI_DOUBLE, y, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		}
+
+		std::cout << "Results received. About to compute norm difference... " << std::endl;
+
+		// Compute norm difference between matrices
+		difference = sqrt(computeSquaredNormDifference(oldMatrix, matrix, numRows));
+
+		// Print stuff
 		std::cout << "Iteration #" << iter << std::endl;
 		std::cout << "Difference: " << difference << std::endl;
-		printSquareMatrix(matrix, numRows, "");
+		printSquareMatrix(matrix, numRows, "new matrix");
+		// printSquareMatrix(oldMatrix, numRows, "old matrix");
 
-		//*** Receive the results and deal with them here (and while loop with difference ?) ***
-	} while (difference > 1.0e-2);
+		/*char test;
+		std::cin >> test;*/
+	} while (difference > 1.0e-2 && iter < 100);
+
+	std::cout << "OUT OF THE LOOP" << std::endl;
+	for (auto y = 0U; y < numRows - 2; ++y)
+	{
+		std::cout << "About to send a null buffer";
+		MPI_Send(nullptr, 0, MPI_DOUBLE, y + 1, 0, MPI_COMM_WORLD);
+		std::cout << "null was sent.";
+	}
+	std::cout << "THE END";
+
 	delete[] matrix;
+	delete[] oldMatrix;
 }
 
 void runWorker(uint32_t, uint32_t)
@@ -110,29 +119,36 @@ void runWorker(uint32_t, uint32_t)
 	// Get matrix rows to process
 	auto numRows = numProcesses + 1;
 	auto * input = new double[3 * numRows];
-	MPI_Recv(input, 3 * numRows, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	bool jobsDone = false;
 
-	//std::cout << *(input - (3 * numRows));
-
-	// Null input means computation is over
-	if (input == nullptr)
+	while (!jobsDone)
 	{
-		return;
+		MPI_Recv(input, 3 * numRows, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		//std::cout << *(input - (3 * numRows));
+
+		// Null input means computation is over
+		if (input == nullptr)
+		{
+			std::cout << "input null !";
+			jobsDone = true;
+			return;
+		}
+
+		// Computation
+		auto * result = new double[numRows - 2];
+		for (auto i = 1U; i < numRows - 1; ++i)
+		{
+			result[i - 1] = (input[i] +
+				input[numRows + i + 1] +
+				input[2 * numRows + i] +
+				input[numRows + i - 1]) / 4.0;
+		}
+
+		// Send result back
+		MPI_Send(result, numRows - 2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+
 	}
-
-	// Computation
-	auto * result = new double[numRows - 2];
-	for (auto i = 1U; i < numRows - 1; ++i)
-	{
-		result[i - 1] = (input[i] +
-			input[numRows + i + 1] +
-			input[2 * numRows + i] +
-			input[numRows + i - 1]) / 4.0;
-	}
-
-	// Send result back
-	MPI_Send(result, numRows - 2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-
 	delete[] input;
 }
 
